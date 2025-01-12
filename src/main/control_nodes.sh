f1b4aaf4637b1a0eb6e6015928b00f78753a1018
Kross Aleksandr <krossale@fel.cvut.cz>
2025-01-12 01:14:53 +0100

    Add more joins

--- src/main/control_nodes.sh
+++ src/main/control_nodes.sh
@@ -3,26 +3,30 @@ source bash_variables.sh
 
 SLEEP_TIME=1
 
+curl http://${NODE_IP[1]}:${NODE_API_PORT[1]}/join/${NODE_IP[2]}/${NODE_PORT[2]}
+sleep ${SLEEP_TIME}
+curl http://${NODE_IP[1]}:${NODE_API_PORT[1]}/join/${NODE_IP[3]}/${NODE_PORT[3]}
+sleep ${SLEEP_TIME}
+curl http://${NODE_IP[1]}:${NODE_API_PORT[1]}/join/${NODE_IP[4]}/${NODE_PORT[4]}
+sleep ${SLEEP_TIME}
 curl http://${NODE_IP[2]}:${NODE_API_PORT[2]}/join/${NODE_IP[1]}/${NODE_PORT[1]}
 sleep ${SLEEP_TIME}
 curl http://${NODE_IP[2]}:${NODE_API_PORT[2]}/join/${NODE_IP[3]}/${NODE_PORT[3]}
 sleep ${SLEEP_TIME}
 curl http://${NODE_IP[2]}:${NODE_API_PORT[2]}/join/${NODE_IP[4]}/${NODE_PORT[4]}
 sleep ${SLEEP_TIME}
-#curl http://${NODE_IP[2]}:${NODE_API_PORT[2]}/join/${NODE_IP[5]}/${NODE_PORT[5]}
-#sleep ${SLEEP_TIME}
 curl http://${NODE_IP[3]}:${NODE_API_PORT[3]}/join/${NODE_IP[1]}/${NODE_PORT[1]}
+sleep ${SLEEP_TIME}
+curl http://${NODE_IP[3]}:${NODE_API_PORT[3]}/join/${NODE_IP[2]}/${NODE_PORT[2]}
 sleep ${SLEEP_TIME}
 curl http://${NODE_IP[3]}:${NODE_API_PORT[3]}/join/${NODE_IP[4]}/${NODE_PORT[4]}
 sleep ${SLEEP_TIME}
-#curl http://${NODE_IP[3]}:${NODE_API_PORT[3]}/join/${NODE_IP[5]}/${NODE_PORT[5]}
-#sleep ${SLEEP_TIME}
 curl http://${NODE_IP[4]}:${NODE_API_PORT[4]}/join/${NODE_IP[1]}/${NODE_PORT[1]}
 sleep ${SLEEP_TIME}
-#curl http://${NODE_IP[4]}:${NODE_API_PORT[4]}/join/${NODE_IP[5]}/${NODE_PORT[5]}
-#sleep ${SLEEP_TIME}
-#curl http://${NODE_IP[6]}:${NODE_API_PORT[6]}/join/${NODE_IP[1]}/${NODE_PORT[1]}
-#sleep ${SLEEP_TIME}
+curl http://${NODE_IP[4]}:${NODE_API_PORT[4]}/join/${NODE_IP[2]}/${NODE_PORT[2]}
+sleep ${SLEEP_TIME}
+curl http://${NODE_IP[4]}:${NODE_API_PORT[4]}/join/${NODE_IP[3]}/${NODE_PORT[3]}
+sleep ${SLEEP_TIME}
 ##
 curl http://${NODE_IP[1]}:${NODE_API_PORT[1]}/get_status
 sleep ${SLEEP_TIME}
